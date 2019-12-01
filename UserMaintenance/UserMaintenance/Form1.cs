bb1610c4f053a7fa1efa9005d2c3d51e7d0838ad
István Trejer <istvan.trejer@stud.uni-corvinus.hu>
2019-12-01 13:21:36 +0100

    User

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();    
+
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +22,22 @@ namespace UserMaintenance
             label2.Text = Resource1.FisrtName;
             button1.Text = Resource1.Add;
 
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+
+
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }
